2e1e607b395f52e6432edd7e70608d2ae10e4201
Kishorer <73980933+kishorer03@users.noreply.github.com>
2025-01-25 17:23:11 +0530

    Time: 3 ms (9.61%) | Memory: 41.2 MB (81.53%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int fun(int[] nums,int k,int ans){
+        int count=0,sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum>ans){
+                ++count;
+                sum=nums[i];
+            }
+        }
+        ++count;
+        System.out.println(count);
+        if(count==k){
+            return 0;
+        }else if(count>k){
+            return -1;
+        }else{
+            return 1;
+        }
+    }
+    public int splitArray(int[] nums, int k) {
+        int i=Integer.MIN_VALUE,j=0,mid,ret=0,temp;
+        if(k>nums.length){
+            return -1;
+        }
+        for(int kk:nums){
+            j+=kk;
+            i=i>kk?i:kk;
+        }
+        while(i<=j){
+            mid=(i+j)/2;
+            temp=fun(nums,k,mid);
+            if(temp==0){
+                ret=mid;
+                j=mid-1;
+            }else if(temp==1){
+                ret=mid;
+                j=mid-1;
+            }else{
+                i=mid+1;
+            }
+        }
+        return ret;
+    }
+}
