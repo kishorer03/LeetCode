98a490fdbe3b88b793b24fe60ed23f1ff824eaca
Kishorer <73980933+kishorer03@users.noreply.github.com>
2025-01-29 23:35:14 +0530

    Time: 23 ms (12.59%) | Memory: 42.3 MB (84.33%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -4,19 +4,18 @@ class Solution {
         Map<Character,Character> mp2=new HashMap<>();
 
         for(int i=0;i<s.length();i++){
-            char ch1=mp1.getOrDefault(s.charAt(i),'Q');
-            if(ch1=='Q'){
+            if(!mp1.containsKey(s.charAt(i))){
                 mp1.put(s.charAt(i),t.charAt(i));
             }else{
-                if(ch1!=t.charAt(i)){
+                if(mp1.get(s.charAt(i))!=t.charAt(i)){
                     return false;
                 }
             }
-            char ch2=mp2.getOrDefault(t.charAt(i),'Q');
-            if(ch2=='Q'){
+
+            if(!mp2.containsKey(t.charAt(i))){
                 mp2.put(t.charAt(i),s.charAt(i));
             }else{
-               if(ch2!=s.charAt(i)){
+               if(mp2.get(t.charAt(i))!=s.charAt(i)){
                     return false;
                 }
             }
