11757e087964838a148d226bd7678a64ef09e166
Kishorer <73980933+kishorer03@users.noreply.github.com>
2025-02-02 18:21:04 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (67.39%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,38 +10,53 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head=new ListNode(0,null);
-        ListNode temp=head;
-        int carry=0,sum=0,rem=0;
-        while(l1!=null || l2!=null){
-            if(l1!=null && l2!=null){
-                sum=l1.val+l2.val+carry;
-                rem=sum%10;
-                carry=sum/10;
-                temp.next=new ListNode(rem,null);
-                temp=temp.next;
-                l1=l1.next;
-                l2=l2.next;
+        ListNode temp1=l1,temp2=l2;
+        int carry=0;
+        while(temp1!=null && temp2!=null){
+            temp1.val=temp1.val+temp2.val+carry;
+            carry=temp1.val/10;
+            temp1.val%=10;
+            temp2.val=temp1.val;
+            if(temp1.next==null && temp2.next==null){
+                if(carry==1){
+                        temp1.next=new ListNode(1,null);
+                        carry=0;
+                    }
             }
-            else if(l1==null && l2!=null){
-                sum=l2.val+carry;
-                rem=sum%10;
-                carry=sum/10;
-                temp.next=new ListNode(rem,null);
-                temp=temp.next;
-                l2=l2.next;
-            }else if(l1!=null && l2==null){
-                sum=l1.val+carry;
-                rem=sum%10;
-                carry=sum/10;
-                temp.next=new ListNode(rem,null);
-                temp=temp.next;
-                l1=l1.next;
-            }            
+            temp1=temp1.next;
+            temp2=temp2.next;
         }
-        if(carry>0){
-            temp.next=new ListNode(carry,null);
+        if(temp1!=null){
+            while(temp1!=null){
+                temp1.val+=carry;
+                carry=temp1.val/10;
+                temp1.val%=10;
+                if(temp1.next==null){
+                    if(carry==1){
+                        temp1.next=new ListNode(1,null);
+                        carry=0;
+                    }
+                }
+                temp1=temp1.next;
+            }
+            return l1;
         }
-        return head.next;
+        if(temp2!=null){
+            while(temp2!=null){
+                temp2.val+=carry;
+                carry=temp2.val/10;
+                temp2.val%=10;
+                if(temp2.next==null){
+                    if(carry==1){
+                        temp2.next=new ListNode(1,null);
+                        carry=0;
+                    }
+                }
+                temp2=temp2.next;
+            }
+            return l2;
+        }
+        return l1;
+        
     }
 }
