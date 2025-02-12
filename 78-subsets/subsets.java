de1197ba92b20657a7e65b98a2cccbd4cfb49dad
Kishorer <73980933+kishorer03@users.noreply.github.com>
2025-02-12 21:55:13 +0530

    Time: 1 ms (54.24%) | Memory: 43 MB (30.81%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,18 +1,16 @@
 class Solution {
-    public void generate(List<Integer> inner,List<List<Integer>> outer,int i,int[] arr){
-        if(i==arr.length){
-            outer.add(new ArrayList<Integer>(inner));
-            return;
-        }
-        inner.add(arr[i]);
-        
-        generate(inner,outer,i+1,arr);
-        inner.remove(inner.size()-1);
-        generate(inner,outer,i+1,arr);
-    }
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> lst=new ArrayList<List<Integer>>();
-        generate(new ArrayList<Integer>(),lst,0,nums);
-        return lst;
+        List<List<Integer>> outer=new ArrayList<List<Integer>>();
+        int len=1<<nums.length;
+        for(int i=0;i<len;i++){
+            List<Integer> inner=new ArrayList<>();
+            for(int j=0;j<nums.length;j++){
+                if((i&(1<<j))!=0){
+                    inner.add(nums[j]);
+                }
+            }
+            outer.add(inner);
+        }
+        return outer;
     }
 }
