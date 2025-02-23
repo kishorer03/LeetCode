d939d4f8d9f3f180780b8d4046ef113700b96292
Kishorer <73980933+kishorer03@users.noreply.github.com>
2025-02-23 14:42:41 +0530

    Time: 4 ms (40.63%) | Memory: 44.6 MB (12.20%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -15,13 +15,12 @@ class Solution {
             }
         }
         int[] out=new int[nums1.length];
+        Map<Integer,Integer> mp=new HashMap<>();
+        for(int i=0;i<nums2.length;i++){
+            mp.put(nums2[i],i);
+        }
         for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                    out[i]=mont[j];
-                    break;
-                }
-            }
+            out[i]=mont[mp.get(nums1[i])];
         }
         return out;
     }
