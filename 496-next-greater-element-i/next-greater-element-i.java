ccdb0be093ccf3b52d59742e1a27d4c1e46bf994
Kishorer <73980933+kishorer03@users.noreply.github.com>
2025-02-23 14:43:53 +0530

    Time: 4 ms (40.63%) | Memory: 44.6 MB (18.02%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> st=new Stack<Integer>();
+        Map<Integer,Integer> mp=new HashMap<>();
         int[] mont=new int[nums2.length];
         for(int i=nums2.length-1;i>=0;i--){
             if(st.empty()){
@@ -13,12 +14,9 @@ class Solution {
                 mont[i]=st.size()>0?st.peek():-1;
                 st.push(nums2[i]);
             }
-        }
-        int[] out=new int[nums1.length];
-        Map<Integer,Integer> mp=new HashMap<>();
-        for(int i=0;i<nums2.length;i++){
             mp.put(nums2[i],i);
         }
+        int[] out=new int[nums1.length];
         for(int i=0;i<nums1.length;i++){
             out[i]=mont[mp.get(nums1[i])];
         }
